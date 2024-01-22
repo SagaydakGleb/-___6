93f5dbf74c5ad2858181ed5beaad7b32a2150b43
Глеб <@HOME-PC>
2024-01-22 19:35:34 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a=int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //if (a == b)
+            //{
+            //    Console.WriteLine("число рано");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("не раны");
+            //}
+            //Console.ReadKey();
+
+            //Задание 2
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());
+            //if(a==b&a==c&b==c)
+            //{
+            //    Console.WriteLine("YES");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NO");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
